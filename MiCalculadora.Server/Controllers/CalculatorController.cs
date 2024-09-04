0f937f1c49e6c009c29c3377147a267a6ee554d5
eliu cortes <eliu.cortes@intercase.com.mx>
2024-09-03 18:44:11 -0600

    Agregar archivos de proyecto.

---
+++ MiCalculadora.Server/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MiCalculadora.Server.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet("{num1}/{num2}")]
+        public decimal operacion(decimal num1,decimal num2, string pOperacion)
+        {
+            var resultado = 0;
+
+            switch (pOperacion)
+            {
+                case "resta":
+                    resultado= 
+                    break;
+            }
+            
+
+
+
+            return resultado;
+
+        }
+    }
+}
